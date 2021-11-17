9be18f64dbdbe61b87480b6ff186f603d5773d77
Juan Demaestri <44933768+jidemaestri@users.noreply.github.com>
2021-11-17 15:15:57 -0300

    show_density
    quantile filtration

---
+++ show_density.py
@@ -0,0 +1,53 @@
+
+import pandas as pd
+from matplotlib import pyplot as plt
+
+# Load data from a text file
+!wget https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/Data/ml-basics/grades.csv
+df_students = pd.read_csv('grades.csv',delimiter=',',header='infer')
+
+# Remove any rows with missing data
+df_students = df_students.dropna(axis=0, how='any')
+
+# Calculate who passed, assuming '60' is the grade needed to pass
+passes  = pd.Series(df_students['Grade'] >= 60)
+
+# Save who passed to the Pandas dataframe
+df_students = pd.concat([df_students, passes.rename("Pass")], axis=1)
+
+
+# Print the result out into this notebook
+print(df_students)
+
+
+# calculate the 0.01th percentile
+
+q01 = df_students.StudyHours.quantile(0.01)
+
+# Get the variable to examine
+col = df_students[df_students.StudyHours>q01]['StudyHours']
+
+# Call the function
+show_distribution(col)
+
+
+
+def show_density(var_data):
+    fig = plt.figure(figsize=(10,4))
+
+    # Plot density
+    var_data.plot.density()
+
+    # Add titles and labels
+    plt.title('Data Density')
+
+    # Show the mean, median, and mode
+    plt.axvline(x=var_data.mean(), color = 'cyan', linestyle='dashed', linewidth = 2)
+    plt.axvline(x=var_data.median(), color = 'red', linestyle='dashed', linewidth = 2)
+    plt.axvline(x=var_data.mode()[0], color = 'yellow', linestyle='dashed', linewidth = 2)
+
+    # Show the figure
+    plt.show()
+
+# Get the density of StudyHours
+show_density(col)
